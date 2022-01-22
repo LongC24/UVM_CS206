16aec0d9a94b08010a2bfe3dd36d870d78d3478b
LongC24 <chrisday1224@outlook.com>
2022-01-22 17:33:06 -0500

    finish the Simulation https://www.reddit.com/r/ludobots/wiki/simulation

--- Assignment 01 /simulate.py
+++ Assignment 01 /simulate.py
@@ -1,7 +1,13 @@
+import time
 import pybullet as p
+# for i in range(1000):
 physicsClient = p.connect(p.GUI)
+for i in range(10000):
+
+    p.stepSimulation()
+    time.sleep(1/60)
+    #print(i)
 
 p.disconnect()
 
-
-pass
+#
